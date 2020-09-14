83ec50f645959e7b88e2ab03415588a4c8fb87a1
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-14 15:44:12 +0300

    Updates

---
+++ precache-manifest.6e62cdcadef90c3c2fe053e2a0985eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd097a14c0f03968115219d5bd7b4326",
+    "url": "/dataviz-bar-chart-d3/index.html"
+  },
+  {
+    "revision": "68cd8992d4493c542960",
+    "url": "/dataviz-bar-chart-d3/static/css/main.5a3b1b6c.chunk.css"
+  },
+  {
+    "revision": "6517ae2ef348d7f6c7b3",
+    "url": "/dataviz-bar-chart-d3/static/js/2.9ab7c2ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dataviz-bar-chart-d3/static/js/2.9ab7c2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68cd8992d4493c542960",
+    "url": "/dataviz-bar-chart-d3/static/js/main.10056b65.chunk.js"
+  },
+  {
+    "revision": "1c174cef5d83cf125103",
+    "url": "/dataviz-bar-chart-d3/static/js/runtime-main.2451bca3.js"
+  }
+]);
